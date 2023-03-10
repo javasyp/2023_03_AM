71652908971fe2f73bc4411ac16368e1d19dc5af
javasyp <javasyp@gmail.com>
2023-03-10 17:57:53 +0900

    회원가입 기능 구현 - 아이디 중복체크

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -6,49 +6,33 @@ import java.util.Scanner;
 
 public class Main {
 	static List<Article> articles_board = new ArrayList<>();
-	// main과 makeTestData 메소드가 static이기 때문에 static 붙여준다.
+	static List<Member> members_board = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		
 		System.out.println("=== 프로그램 시작 ===");
 		
-//		List<Article> articles_board = new ArrayList<>();
-		// 현재 지역변수인 articles_board를 전역변수로 빼기
-		
-		// 테스트할 데이터 미리 생성하기
 		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
 		
-		// 게시물을 담을 배열 리스트 생성
-		// 주의 ※ 반복문 안에 넣으면 계속 초기화 됨!!!
-		
-		// makeTestData() 호출하여 이미 3개가 들어와 있는 상태
-		int lastId = 3;
-		
-		int count = 0;
+		int lastArticleId = 3;
+		int lastMemberId = 0;
 
 		while (true) {
 			System.out.print("명령어 > ");
 			String command = sc.nextLine();
 
-			// 추가
-			// 아무것도 입력 안 했을 때
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해 주세요.");
 				continue;
 			}
-			// 여기까지
 			
-			// exit 입력 시 종료
 			if (command.equals("exit")) {
 				break;
 			}
 			
-			
 			// 1. 게시판 목록 출력하기
-			// 리스트에 게시글이 있으면 최신순으로 글 내용 출력
-			// 없으면 "게시글이 없습니다." 출력
 			if (command.equals("article list")) {
 				if (articles_board.size() == 0) {
 					
@@ -57,7 +41,6 @@ public class Main {
 				} else {
 					System.out.println("번호 / 제목 / 조회");
 					
-					// 최신순으로 출력하기
 					for (int i = articles_board.size() - 1; i >= 0; i--) {
 						
 						Article articles_list = articles_board.get(i); // 값 읽어와서 변수에 저장
@@ -67,9 +50,8 @@ public class Main {
 			}
 			
 			// 2. 게시글 입력하기
-			// 제목과 내용을 배열 리스트에 저장
 			else if (command.equals("article write")) {
-				int id = lastId + 1;
+				int id = lastArticleId + 1;
 
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
@@ -77,58 +59,34 @@ public class Main {
 				System.out.print("내용 : ");
 				String content = sc.nextLine();
 				
-				// 작성 날짜 저장
 				String regDate = Util.getNowDateTimeStr();
 
-//				Article article = new Article(id, title, content);
-//				articles_board.add(article);
-				articles_board.add(new Article(id, regDate, regDate, title, content, count));
+				Article article = new Article(id, regDate, regDate, title, content);
+				articles_board.add(article);
 
 				System.out.println(id + "번 글이 생성되었습니다.");
-				lastId++;
+				lastArticleId++;
 				
 			}
 			
 			// 3. 게시글의 세부사항 출력하기
-			// "article detail 번호" 입력 시 없으면 "게시물이 존재하지 않습니다."
-			// 있으면 번호, 날짜, 제목, 내용 출력
-			else if (command.startsWith("article detail")) {				
-				// "article" "delete" "1" 로 쪼개어 저장
+			else if (command.startsWith("article detail")) {
 				String[] cmdDiv = command.split(" ");
-				// cmdDiv[0] : article
-				// cmdDiv[1] : delete
-				// cmdDiv[2] : (숫자 부분)
 				
-				// 몇 번인지 확인이 안될 때
 				if (cmdDiv.length < 3) {
 					System.out.println("명령어를 확인해 주세요.");
 					continue;
 				}
 				
-				// 문자열을 정수형으로 형 변환
 				int num = Integer.parseInt(cmdDiv[2]);
-				
 				
 				Article foundArticle = getArticleId(num);
 				
-				// 게시물 있는지 판별 (배열 순회하여 비교)
-				// 중복 구간 시작!! (중복 코드 -> 메소드 만들기)
-//				for (int i = 0; i < articles_board.size(); i++) {
-//					Article articles_detail = articles_board.get(i);
-//					
-//					if (articles_detail.ID == num) {
-//						foundArticle = articles_detail;
-//						break;		// 찾았으면 그만 찾아~
-//					}
-//				}
-				// 중복 구간 끝!!
-				
-				if (foundArticle == null) {	// 게시물 없는 경우
+				if (foundArticle == null) {
 					System.out.println(num + "번 게시물은 존재하지 않습니다.");
 					continue;
 				}
 				
-				// 상세보기 할 때마다 조회수 증가
 				foundArticle.Count++;
 				
 				System.out.println("번호 : " + foundArticle.ID);
@@ -137,13 +95,9 @@ public class Main {
 				System.out.println("제목 : " + foundArticle.Title);
 				System.out.println("내용 : " + foundArticle.Content);
 				System.out.println("조회수 : " + foundArticle.Count);
-				
 			}
 			
-			
 			// 4. 게시글 삭제하기
-			// "article delete 번호" 입력 시 없으면 "n번 게시물이 존재하지 않습니다."
-			// 있으면 삭제 후 "n번 게시물이 삭제되었습니다." 출력
 			else if (command.startsWith("article delete")) {
 				String[] cmdDiv = command.split(" ");
 				
@@ -153,14 +107,10 @@ public class Main {
 				}
 				
 				int num = Integer.parseInt(cmdDiv[2]);
-				
-				// 값을 삭제하면 삭제한 만큼 인덱스는 하나씩 줄게 된다.
-				// 그래서 내가 찾는 글의 번호와 실제 인덱스의 값이 일치하지 않는다.
-				// 삭제 후 저장할 때 인덱스는 난 자리부터 채워지기 때문에
-				// 반드시 변수를 따로 지정해준다.
+
 				int foundIndex = getArticleIndex(num);
 				
-				if (foundIndex == -1) {	// 게시물 없는 경우
+				if (foundIndex == -1) {
 					System.out.println(num + "번 게시물은 존재하지 않습니다.");
 					continue;
 				}
@@ -168,13 +118,9 @@ public class Main {
 				articles_board.remove(foundIndex); 
 				
 				System.out.println(num + "번 글이 삭제되었습니다.");
-				
 			}
 			
-			
-			// 5. 게시글 수정하기 - 3번 상세보기와 유사함
-			// "article modify 번호" 입력 시 없으면 "n번 게시물은 존재하지 않습니다."
-			// 있으면 수정된 내용 출력 후 "n번 게시물이 수정되었습니다." 출력
+			// 5. 게시글 수정하기
 			else if (command.startsWith("article modify")) {
 				
 				String[] cmdDiv = command.split(" ");
@@ -188,7 +134,7 @@ public class Main {
 				
 				Article foundArticle = getArticleId(num);
 				
-				if (foundArticle == null) {	// 게시물 없는 경우
+				if (foundArticle == null) {
 					System.out.println(num + "번 게시물은 존재하지 않습니다.");
 					continue;
 				}
@@ -206,7 +152,42 @@ public class Main {
 				foundArticle.UpdateDate = update_date;
 				
 				System.out.println(num + "번 글이 수정되었습니다.");
+			}
+			
+			// 6. 회원가입
+			else if (command.equals("member join")) {
+				int id = lastMemberId + 1;
+				String regdate = Util.getNowDateTimeStr();
 				
+				String join_id = null; 
+				
+				
+				// 중복 검사
+				while (true) {
+					System.out.print("아이디 : ");
+					join_id = sc.nextLine();
+					
+					if (isJoinableId(join_id) == false) {
+						System.out.println("이미 사용중인 아이디입니다");
+						continue;
+					}
+					break;
+				}
+				
+				System.out.print("비밀번호 : ");
+				String join_pw = sc.nextLine();
+				
+//				System.out.print("비밀번호 확인 : ");
+//				String join_pw_ck = sc.nextLine();
+				
+				System.out.print("이름 : ");
+				String join_name = sc.nextLine();
+				
+				Member members = new Member(id, regdate, regdate, join_id, join_pw, join_name);
+				members_board.add(members);
+				
+				System.out.println(id + "회원가입이 되었습니다.");
+				lastMemberId++;
 			}
 			
 			else {
@@ -219,16 +200,29 @@ public class Main {
 		sc.close();
 	}
 	
-	// 4
+	// 중복체크
+	private static boolean isJoinableId(String loginId) {
+		int index = getMemberIndex(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static int getMemberIndex(String loginId) {
+		int i = 0;
+		for (Member member : members_board) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
 	private static int getArticleIndex(int num) {
-//		for (int i = 0; i < articles_board.size(); i++) {
-//			Article articles_find = articles_board.get(i);
-//			if (articles_find.ID == num) {
-//				return i;
-//			}
-//		}
-//		return -1;
-		
 		int i = 0;
 		for (Article articles_find : articles_board) {
 			if (articles_find.ID == num) {
@@ -239,25 +233,7 @@ public class Main {
 		return -1;
 	}
 	
-	
-	// 3, 5 중복 코드
 	private static Article getArticleId(int num) {
-//		for (int i = 0; i < articles_board.size(); i++) {
-//			Article articles_find = articles_board.get(i);
-//			
-//			if (articles_find.ID == num) {
-//				return articles_find;
-//			}
-//		}
-//		return null;
-		
-//		for (Article articles_find : articles_board) {
-//			if (articles_find.ID == num) {
-//				return articles_find;
-//			}
-//		}
-//		return null;
-		
 		int index = getArticleIndex(num);
 		
 		if (index != -1) {
@@ -265,31 +241,42 @@ public class Main {
 		}
 		
 		return null;
-		
 	}
 	
-
-
-	// 단어 미리 입력하는 함수
-	public static void makeTestData() {
+	private static void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
 		articles_board.add(new Article(1, Util.getNowDateTimeStr(), Util.getNowDateTimeStr(), "제목1", "내용1", 11));
 		articles_board.add(new Article(2, Util.getNowDateTimeStr(), Util.getNowDateTimeStr(), "제목2", "내용2", 22));
 		articles_board.add(new Article(3, Util.getNowDateTimeStr(), Util.getNowDateTimeStr(), "제목3", "내용3", 33));
 	}
+}
+
+class Member {
+	int ID;
+	String RegDate;
+	String UpdateDate;
+	String loginId;
+	String loginPw;
+	String name;
 	
+	Member(int id, String regDate, String updateDate, String loginid, String loginpw, String name) {
+		this.ID = id;
+		this.RegDate = regDate;
+		this.UpdateDate = updateDate;
+		this.loginId = loginid;
+		this.loginPw = loginpw;
+		this.name = name;
+	}
 }
 
 class Article {
-	int ID;		// 글 번호
-	int Count;	// 글 조회수
-	String RegDate;		// 입력날짜
-	String UpdateDate;	// 수정날짜
-	String Title;		// 제목
-	String Content;		// 내용
+	int ID;
+	int Count;
+	String RegDate;
+	String UpdateDate;
+	String Title;
+	String Content;
 	
-	// 조회수 X
-	// 메소드 오버로딩
 	Article(int id, String regDate, String updateDate, String title, String content) {
 		this(id, regDate, updateDate, title, content, 0);
 	}
